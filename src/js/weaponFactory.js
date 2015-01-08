23bff7afa10953251e09e5f9b62dea0575f425d6
Kristofer Fagerström <sonicaddiction@gmail.com>
2015-01-08 10:53:09 +0100

    Weapon: add name to gun

--- src/js/weaponFactory.js
+++ src/js/weaponFactory.js
@@ -6,7 +6,8 @@ function WeaponFactory(game) {
 
 WeaponFactory.prototype.createPistol = function () {
 	var gun = new Gun(this.game, 'bullet', 1000, 1000, 0.5);
-	
+	gun.name = 'Pistol';
+
 	return gun;
 }
 
